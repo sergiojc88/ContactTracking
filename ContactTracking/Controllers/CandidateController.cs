3e47afa25c0e2000c10eeed06f4eae653a3567ff
sergiojc88 <99311523+sergiojc88@users.noreply.github.com>
2022-02-09 01:31:45 -0400

    Add files via upload

---
+++ ContactTracking/Controllers/CandidateController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ContactTracking.Models;
+using ContactTracking.Repository;
+using System.Web.Http.Cors;
+using System.ComponentModel.DataAnnotations;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ContactTracking.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    [Route("api/[controller]")]
+    public class CandidateController : Controller
+    {
+        // GET: api/<controller>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            RPCandidate rpCli = new RPCandidate();
+            return Ok(rpCli.ObtenerClientes());
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            RPCandidate rpCli = new RPCandidate();
+
+            var cliRet = rpCli.ObtenerCliente(id);
+
+            if (cliRet == null)
+            {
+                var nf = NotFound("The candidate " + id.ToString() + " doesnt exist.");
+                return nf;
+            }
+
+            return Ok(cliRet);
+        }
+
+        
+        [HttpGet("search/id={id}/firstname={firstname}/lastname={lastname}/emailaddress={emailaddress}/phonenumber={phonenumber}/residentialzipcode={residentialzipcode}")]
+        public IActionResult Search(int id, string firstname, string lastname, string emailaddress, string phonenumber, string residentialzipcode)
+        {
+            RPCandidate rpCli = new RPCandidate();
+
+            var cliRet = rpCli.searchCandidate(id, firstname, lastname, emailaddress, phonenumber, residentialzipcode);
+
+            if (cliRet == null)
+            {
+                var nf = NotFound("The candidate " + id.ToString() + " doesnt exist.");
+                return nf;
+            }
+
+            return Ok(cliRet);
+        }
+
+        [HttpPost("agregar")]
+        public IActionResult AgregarCliente([FromBody] Candidate nuevoCliente)
+        {
+            if(!validateEmail(nuevoCliente.EmailAddress))
+            {
+                var nf = BadRequest("The email is not correct.");
+                return nf;
+            }
+            RPCandidate rpCli = new RPCandidate();
+            rpCli.Agregar(nuevoCliente);
+            Console.WriteLine(nuevoCliente.FirstName);
+            return CreatedAtAction(nameof(AgregarCliente), nuevoCliente);
+        }
+
+        static bool validateEmail(string email)
+        {
+            if (email == null)
+            {
+                return false;
+            }
+            if (new EmailAddressAttribute().IsValid(email))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ ContactTracking/Repository/RPCandidate.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ContactTracking.Models;
+
+namespace ContactTracking.Repository
+{
+    public class RPCandidate
+    {
+        public static List<Candidate> _listCandidates = new List<Candidate>()
+        {
+            new Candidate() { Id = 1, FirstName = "Silvester" , LastName = "Apellido 1", EmailAddress = "canditate@gmail.com", PhoneNumber="15346", ResidentialZipCode = "fort lauderdale 12" },
+            new Candidate() { Id = 2, FirstName = "Jhon" , LastName = "Apellido 2", EmailAddress = "jhon@gmail.com", PhoneNumber="142357", ResidentialZipCode = "Florida 12" },
+            new Candidate() { Id = 3, FirstName = "Bryan" , LastName = "Apellido 3", EmailAddress = "bryan@gmail.com", PhoneNumber="9523356", ResidentialZipCode = "Silversprings 12" }
+        };
+
+        public IEnumerable<Candidate> ObtenerClientes()
+        {
+            return _listCandidates;
+        }
+
+        public Candidate ObtenerCliente(int id)
+        {
+            var candidate = _listCandidates.Where(cli => cli.Id == id);
+
+            return candidate.FirstOrDefault();
+        }
+
+        public void Agregar(Candidate newCandidate)
+        {
+            _listCandidates.Add(newCandidate);
+        }
+
+        public IEnumerable<Candidate> searchCandidate(int id, string fname, string lname, string email, string phone, string residential)
+        {
+            var candidate = _listCandidates.Where(cli => cli.Id == id || cli.FirstName == fname || cli.LastName == lname || cli.EmailAddress == email || cli.PhoneNumber == phone || cli.ResidentialZipCode == residential);
+
+            return candidate;
+        }
+    }
+}
